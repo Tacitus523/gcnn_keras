d706518208805ab3e7cc610d33e8d8ac9df8e2ac
Lukas Petersen <lukas.petersen@kit.edu>
2024-06-14 17:29:36 +0200

    serializable learning rate from callback

--- kgcnn/utils/callbacks.py
+++ kgcnn/utils/callbacks.py
@@ -3,6 +3,6 @@ import tensorflow as tf
 class LearningRateLoggingCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         optimizer = self.model.optimizer
-        current_lr = optimizer.lr.numpy()
+        current_lr = optimizer.lr.numpy().item() # get the current learning rate, JSON serializable
         if logs is not None:
             logs["lr"] = current_lr
