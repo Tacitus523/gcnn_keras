73db574dc9c6e128e25e97820d1b30d3976aaf19
Lukas Petersen <lukas.petersen@kit.edu>
2025-05-11 19:36:19 +0200

    Add save_extxyz function to save ASE Atoms objects to extxyz files

--- kgcnn/utils/save_load_utils.py
+++ kgcnn/utils/save_load_utils.py
@@ -1,6 +1,9 @@
 import os
 import pickle
 import numpy as np
+from ase import Atoms
+from ase.io import read, write
+from typing import List, Dict, Tuple
 
 def save_history(history_objects: list, filename: str = "histories.pkl"):
     """_summary_
@@ -46,5 +49,54 @@ def load_training_indices(filename: str ="training_indices.pkl", expected_length
 
     return train_indices, test_indices
 
+def save_extxyz(atomic_numbers_list: List[np.ndarray],
+                positions_list: List[np.ndarray],
+                ref_infos: Dict[str, np.ndarray],
+                ref_arrays: Dict[str, np.ndarray],
+                pred_infos: Dict[str, np.ndarray],
+                pred_arrays: Dict[str, np.ndarray],
+                filename: str = "geoms.extxyz") -> None:
+    """Creates ase.Atoms object from the given data and saves it to an extxyz file.
+    Args:
+        atomic_numbers_list (List[np.ndarray]): List of atomic numbers.
+        positions_list (List[np.ndarray]): List of atomic positions.
+        ref_infos (Dict[str, np.ndarray]): Reference information dictionary.
+        ref_arrays (Dict[str, np.ndarray]): Reference arrays dictionary.
+        pred_infos (Dict[str, np.ndarray]): Predicted information dictionary.
+        pred_arrays (Dict[str, np.ndarray]): Predicted arrays dictionary.
+        filename (str, optional): Filename for the output extxyz file. Defaults to "geoms.extxyz".
+    """
+    atoms_list = []
     
+    for i in range(len(atomic_numbers_list)):
+        atomic_numbers = atomic_numbers_list[i]
+        positions = positions_list[i]
+        
+        # Create ASE Atoms object
+        atoms = Atoms(numbers=atomic_numbers, positions=positions)
+        
+        # Add reference information to Atoms object
+        for key, val in ref_infos.items():
+            if i < len(val):
+                atoms.info[f"ref_{key}"] = val[i]
+        
+        # Add predicted information to Atoms object
+        for key, val in pred_infos.items():
+            if i < len(val):
+                atoms.info[f"pred_{key}"] = val[i]
+                
+        # Add reference arrays to Atoms object
+        for key, val in ref_arrays.items():
+            if i < len(val):
+                atoms.arrays[f"ref_{key}"] = val[i]
+                
+        # Add predicted arrays to Atoms object
+        for key, val in pred_arrays.items():
+            if i < len(val):
+                atoms.arrays[f"pred_{key}"] = val[i]
+                
+        atoms_list.append(atoms)
+    
+    # Write Atoms objects to extxyz file
+    write(filename, atoms_list, format='extxyz')
     
